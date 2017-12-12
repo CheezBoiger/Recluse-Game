28ad19b6a59fe298f39dcd05acbb018f0e586291
Mario Garcia <alexander.garcia18@yahoo.com>
2017-12-11 21:49:18 -0800

    Bloom IS NOW behaved.

--- Engine/Renderer/Private/Renderer.cpp
+++ Engine/Renderer/Private/Renderer.cpp
@@ -1613,7 +1613,7 @@ void Renderer::BuildHDRCmdBuffer(u32 cmdBufferIndex)
 
   cmdBuffer->Begin(cmdBi);
     // TODO(): Need to allow switching on/off bloom passing.
-    m_Downscale.strength = 0.4f;
+    m_Downscale.strength = 0.45f;
     m_Downscale.scale = 4.0f;
     m_Downscale.horizontal = true;
     VkDescriptorSet DownscaleSetNative = DownscaleSet2x->Handle();
